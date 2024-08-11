9d28af69b6d93af9c3dde94d045a3ff06681f4f3
Mialy Ravelomihaja <124779250+mialyravelomihaja@users.noreply.github.com>
2024-08-11 21:15:57 +0100

    création du projet react

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <div className='App'>
+          <h1>Hello World</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
