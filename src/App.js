1384647f240003be50bc6ae7c85aa68cb334377e
Mialy Ravelomihaja <124779250+mialyravelomihaja@users.noreply.github.com>
2024-08-11 21:53:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Stack from 'react-bootstrap/Stack';
 
 function App() {
   return (
